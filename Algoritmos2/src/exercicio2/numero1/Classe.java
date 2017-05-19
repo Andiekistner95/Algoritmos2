3076484c8f04510126ad37d9363dbfefcaab83cf
Noturno <Noturno@192.168.7.21>
2017-05-18 21:58:07 -0300

    commit inicial

---
+++ Algoritmos2/src/exercicio2/numero1/Classe.java
@@ -0,0 +1,13 @@
+package exercicio2.numero1;
+
+public class Classe {
+
+	public static void main(String[] args) {
+
+		int quantidade = 10;
+		for (int aux = 0; aux <= quantidade; aux++) {
+				System.out.println(aux);
+				
+		}
+	}
+}
---
+++ Algoritmos2/src/exercicio2/numero2/Classe.java
@@ -0,0 +1,14 @@
+package exercicio2.numero2;
+
+public class Classe {
+
+	public static void main(String[] args) {
+		
+		int quantidade = 10;
+		
+		for( int aux = quantidade; aux >= -1; aux--){
+			System.out.println(aux);
+		}
+			
+	}
+}
